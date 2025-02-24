af9abedd08993b022373d752abe2666e531f3bdd
Kiran Kumar Kilaru <kilarukirankumar83@icloud.com>
2025-02-24 04:46:21 +0000

    dag changes and removed stage copy

--- de_project_1/app/create_task.py
+++ de_project_1/app/create_task.py
@@ -7,20 +7,27 @@ from snowflake.core.task.dagv1 import DAG, DAGOperation, DAGTask, CreateMode
 from snowflake.snowpark.functions import udf
 import os
 
-connection = snowflake.connector.connect(user=os.environ.get("SNOWFLAKE_USER"), password=os.environ.get("SNOWFLAKE_PASSWORD"),\
-                                         role=os.environ.get("SNOWFLAKE_ROLE"), database=os.environ.get("SNOWFLAKE_DATABASE"),\
-                                         account=os.environ.get("SNOWFLAKE_ACCOUNT"),\
-                                         warehouse=os.environ.get("SNOWFLAKE_WAREHOUSE"),\
-                                        schema="PUBLIC")
+# connection = snowflake.connector.connect(user=os.environ.get("SNOWFLAKE_USER"), password=os.environ.get("SNOWFLAKE_PASSWORD"),\
+#                                          role=os.environ.get("SNOWFLAKE_ROLE"), database=os.environ.get("SNOWFLAKE_DATABASE"),\
+#                                          account=os.environ.get("SNOWFLAKE_ACCOUNT"),\
+#                                          warehouse=os.environ.get("SNOWFLAKE_WAREHOUSE"),\
+#                                         schema="PUBLIC")
 
-# connection = snowflake.connector.connect()
+connection = snowflake.connector.connect()
 root = Root(connection)
 
 with DAG('dag_copy_emp',schedule=timedelta(days=1), warehouse="compute_wh", stage_location="@dev_deployment") as dag:
-    dag_task_1 = DAGTask("copy_from_s3", StoredProcedureCall(procedures.hello_procedure,\
+    dag_task_1 = DAGTask("copy_from_s3", StoredProcedureCall(procedures.copy_to_table_proc,\
                          stage_location="dev_deployment",\
                          packages=['snowflake-snowpark-python'], imports=['@dev_deployment/de_project_1/app.zip']),\
                          warehouse="compute_wh")
+    
+    dag_task_2 = DAGTask("copy_to_emp_tgt", StoredProcedureCall(procedures.copy_to_emp_tgt_proc,\
+                         stage_location="dev_deployment",\
+                         packages=['snowflake-snowpark-python'], imports=['@dev_deployment/de_project_1/app.zip']),\
+                         warehouse="compute_wh")
+    
+    dag_task_1 >> dag_task_2
 
     schema = root.databases["demo_db"].schemas["public"]
     dag_op = DAGOperation(schema=schema)
